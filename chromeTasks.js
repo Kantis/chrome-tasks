cbe848b38ceec620c02e310accca3095672bf5f7
Emil Kantis <e.kantis@gmail.com>
2015-06-09 21:26:08 +0200

    semi-working

--- chromeTasks.js
+++ chromeTasks.js
@@ -5,9 +5,34 @@ var chromeTasks = angular.module('chromeTasks', ['googleApi']);
 
 chromeTasks.controller('MainController', ['tasksApi', '$scope', function (tasksApi, $scope) {
 	$scope.tasklists = [];
+	$scope.tasks = [];
 
-	tasksApi.getTaskLists().then(
-		function (result) {
-			console.log(result);
-		});
+	var init = function() {
+		tasksApi.getTaskLists().then(
+			function (result) {
+				$scope.tasklists =  result.data.items;
+				$scope.selectedList = result.data.items[0].id;
+
+				getTasksForList($scope.selectedList);
+			});
+	}
+
+	$scope.selectList = function (selectedList) {
+		$scope.selectedList = selectedList;
+		getTasksForList(selectedList);
+	}
+
+	$scope.isListSelected = function(tasklistId) {
+		return $scope.selectedList == tasklistId;
+	}
+
+	var getTasksForList = function(taskList) {
+		tasksApi.getTasks($scope.selectedList).then(
+			function (result) {
+				console.log(result);
+				$scope.tasks = result.data.items;
+			});
+	}
+
+	init();
 }]);
--- googleApi.js
+++ googleApi.js
@@ -5,20 +5,28 @@ var GoogleApi = angular.module('googleApi', ['googleAuth']);
 GoogleApi.factory('tasksApi', ['$q', '$http', 'authApi', function ($q, $http, authApi) {
 	var endpointBase = 'https://www.googleapis.com/tasks/v1';
 
-	var getTasks = function() {
-		return ["Do this then that", "Do something else"]
+	var getTasks = function(taskList) {
+		return $q(function (resolve, reject) {
+			authApi.getAccessToken().then(function (result) {
+				$http.defaults.headers.common.Authorization = 'OAuth ' + result;
+				$http.get(endpointBase + '/lists/' + taskList + '/tasks').then(
+					function (result) {
+						resolve(result);
+					});
+			});
+		});
 	};
 
 	var getTaskLists = function() {
 		return $q(function (resolve, reject) {
 			authApi.getAccessToken().then(function (result) {
-				$http.defaults.headers.common.Authorization = 'OAuth ' + result
+				$http.defaults.headers.common.Authorization = 'OAuth ' + result;
 				$http.get(endpointBase + '/users/@me/lists').then(
 					function (result) {
 						resolve(result);
 					});
 			});
-		})
+		});
 	}
 	
 	return {
